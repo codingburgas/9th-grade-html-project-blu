b92959474e3517518b1a05aec5426ecc8ba3df77
RISpasov23 <RISpasov23@codingburgas.bg>
2025-06-09 19:29:09 +0300

    Connect the two maps

--- js/iformationForm.js
+++ js/iformationForm.js
@@ -5,8 +5,16 @@ var map = L.map('mapForm', {
 L.tileLayer('https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=kwy7kbU0521uvJB2Z5c4', {
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
 }).addTo(map);
+var lat = null;
+var lng = null;
 //add and delete markers on click
 map.on("click", function(e)
 {
     var marker = new L.marker([e.latlng.lat, e.latlng.lng]).addTo(map).on('click', e=> e.target.remove());
+    lat = e.latlng.lat;
+    lng = e.latlng.lng;
 });
+function getInformation() {
+  localStorage.setItem(`latitude${localStorage.length}` , String(lat));
+  localStorage.setItem(`longitude${localStorage.length-1}` , String(lng)); 
+};
--- js/map.js
+++ js/map.js
@@ -5,3 +5,11 @@ var map = L.map('map', {
 L.tileLayer('https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=kwy7kbU0521uvJB2Z5c4', {
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
 }).addTo(map);
+for(let i = 0; i < localStorage.length; i+=2)
+{
+    const key1 = localStorage.key(i);
+    const key2 = localStorage.key(i+1);
+    var lat = Number(localStorage.getItem(key1));
+    var lng = Number(localStorage.getItem(key2));
+    var marker = L.marker([lat, lng]).addTo(map);
+}
